4dd08f724783b209a59d3d47d791269469c2a0e7
mmedchuk <m.medchuk@gmail.com>
2022-12-30 17:54:47 +0200

    Admin panel settins were changed

--- src/users/admin.py
+++ src/users/admin.py
@@ -4,3 +4,12 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 admin.register(User)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    exclude = ["groups", "user_permissions"]
+    readonly_fields = ["email", "last_login", "is_staff", "is_active", "password"]
+    list_display = ["email", "first_name", "last_name", "role", "is_active"]
+    list_filter = ["is_active", "role"]
+    search_fields = ["email", "last_name"]
